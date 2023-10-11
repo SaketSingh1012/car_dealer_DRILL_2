c91af929aaf15f96f212b99a94274d4200214e6b
Saket Singh <81484363+SaketSingh1012@users.noreply.github.com>
2023-10-11 21:49:59 +0530

    Problem4 solved

---
+++ problem4.js
@@ -0,0 +1,8 @@
+const inventory = require('./inventory');
+
+function getAllCarYears(inventory) {
+  const carYears = inventory.map((car) => car.car_year);
+  return carYears;
+}
+
+module.exports = getAllCarYears;
---
+++ test/testProblem4.js
@@ -0,0 +1,5 @@
+const getAllCarYears = require('./problem4');
+const inventory = require('./inventory');
+
+const result = getAllCarYears(inventory);
+console.log(result);
