96e173740ca8a5d16e0c2b36f8dceb1fe85643ea
Saket Singh <81484363+SaketSingh1012@users.noreply.github.com>
2023-10-11 21:48:18 +0530

    Problem3 solved

---
+++ problem3.js
@@ -0,0 +1,8 @@
+const inventory = require('./inventory');
+
+function sortCarModelsAlphabetically(inventory) {
+  const sortedModels = [...inventory].sort((a, b) => a.car_model.localeCompare(b.car_model));
+  return sortedModels.map((car) => car.car_model);
+}
+
+module.exports = sortCarModelsAlphabetically;
